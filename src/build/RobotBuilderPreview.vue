9933a0d24dbddaafb2ae7937e946d52e936f9ff5
nehalahmad <nehal.ahmad@nagarro.com>
2019-07-04 09:20:21 +0530

    firt commit

---
+++ src/build/RobotBuilderPreview.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="preview">
+    <div class="preview-content">
+      <div class="top-row">
+        <img :src="selectedRobot.head.src" />
+      </div>
+    </div>
+    <div class="middle-row">
+      <img :src="selectedRobot.leftArm.src" class="rotate-left" />
+      <img :src="selectedRobot.torso.src" />
+      <img :src="selectedRobot.rightArm.src" class="rotate-right" />
+    </div>
+    <div class="bottom-row">
+      <img :src="selectedRobot.base.src" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RobotBuilderPreview"
+};
+</script>
+
+<style>
+</style>
+
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open = !open">&#x25B2; Collapse</span>
+      <span v-if="!open" @click="open = !open">&#x25BC; Expand</span>
+    </div>
+
+    <!-- Toggleable content here -->
+    <slot v-if="open">
+      <div>Default content goes here</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CollapsibleSection",
+  data: () => ({ open: true })
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #bbb;
+  padding: 3px;
+  cursor: pointer;
+  text-align: left;
+}
+</style>
